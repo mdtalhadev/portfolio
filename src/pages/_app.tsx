217dc3551cc2cf2e6cd548cf0337d272bfdd33a1
MdTalha <mdtalhadev@gmail.com>
2024-09-22 20:38:23 +0500

    whatsapp icon attached

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import AppBar from "@/components/navBar";
 import Footer from "@/components/footer";
+import WhatsappIcon from "@/components/whatsappIcon";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
-  <AppBar />
-  <Component {...pageProps} />
-  <Footer />
+    <AppBar />
+    <Component {...pageProps} />
+    <Footer />
+    <WhatsappIcon /> 
+  
   </>;
 }
