5eab19b16ce39532116e1c8a35f351929372470a
MdTalha <mdtalhadev@gmail.com>
2024-09-23 01:10:56 +0500

    Featured Projects font size fixes for mobile

--- src/components/home/projects.tsx
+++ src/components/home/projects.tsx
@@ -52,7 +52,7 @@ const ProjectsSection = () => {
     return (
         <section className="py-4">
             <div className="container mx-auto px-4">
-                <h2 className="text-4xl font-bold mb-8 text-center">🚀 Featured Projects</h2>
+                <h2 className="text-2xl sm:text-3xl md:text-4xl  lg:text-4xl mxl:text-4xl font-bold mb-8 text-center">🚀 Featured Projects</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
                     {
                         projects.map((project) => {
