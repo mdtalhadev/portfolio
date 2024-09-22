2f6f6b23d612768bba677e7f004b254e6b07262c
MdTalha <mdtalhadev@gmail.com>
2024-09-23 00:45:20 +0500

    title and icon updated

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <meta name="description" content="My portfolio website" />
+        <title>Md Talha</title>
+        <link rel="icon" href="/images/profile.jpg" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
