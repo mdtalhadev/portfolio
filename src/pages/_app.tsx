a17c846f7fad71fc0bb0415267d1ee8ecdf3af81
MdTalha <mdtalhadev@gmail.com>
2024-09-22 20:12:17 +0500

    basic footer Ui is ready

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import AppBar from "@/components/navBar";
+import Footer from "@/components/footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
   <AppBar />
   <Component {...pageProps} />
+  <Footer />
   </>;
 }
