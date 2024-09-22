0574f85e5c6e31ed3828c1632dddecc315aa1688
MdTalha <mdtalhadev@gmail.com>
2024-09-23 00:45:08 +0500

    projects section is responsive

--- src/components/home/projects.tsx
+++ src/components/home/projects.tsx
@@ -50,7 +50,7 @@ const ProjectsSection = () => {
 
 
     return (
-        <section className="py-16">
+        <section className="py-4">
             <div className="container mx-auto px-4">
                 <h2 className="text-4xl font-bold mb-8 text-center">🚀 Featured Projects</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
@@ -61,7 +61,7 @@ const ProjectsSection = () => {
                     }
 
                 </div>
-                <div className="w-full text-end text-blue-800 ">
+                <div className="w-full text-end text-blue-800 mt-5 ">
                     <Link href="/projects">View all projects</Link>
 
                 </div>
